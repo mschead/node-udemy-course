23f84a86fddd2107d5ceba4d00312194257c4dbd
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-16 13:03:41 -0200

    Section 3: Requiring Your Own Files

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,8 +1,11 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-var user = os.userInfo();
+console.log('Result:', notes.add(9, -2));
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}!`);
+// var user = os.userInfo();
+
+// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
