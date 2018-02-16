09bda39d973c31dc6e739ff54f301a133f87470e
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-16 13:23:06 -0200

    Section 3: Using 3rd Party Modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+// console.log(_.isString(true));
+// console.log(_.isString('Schead'));
+var filteredArray = _.uniq(['Schead', 1, 'Schead', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// console.log('Result:', notes.add(9, -2));
 
 // var user = os.userInfo();
 
