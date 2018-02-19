6770a81e2f1ba2f71cd95f3dc8b38f77433e1e23
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-19 16:35:22 -0300

    Section 4: Encoding User Input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
   // Using key from google developer, some changes might be needed!
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyBPHa_STFlfzSDgjPoDOftEX3J_0St0BW4',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBPHa_STFlfzSDgjPoDOftEX3J_0St0BW4`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
