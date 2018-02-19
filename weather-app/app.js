260fd55dd24d66519adeb1688827d1c615594cc9
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-19 15:15:56 -0300

    Section 4: What's Makes up an HTTP Request?

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyBPHa_STFlfzSDgjPoDOftEX3J_0St0BW4',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
