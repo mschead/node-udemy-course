0e1b0c578efc162c23d06af5c0697b810a338b49
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-19 14:31:12 -0300

    Section 4: Pretty Printing Objects

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,5 +5,5 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyBPHa_STFlfzSDgjPoDOftEX3J_0St0BW4',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(JSON.stringify(body, undefined, 2));
 });
