27d638f0b4c01dfc45e8f811e6e6c8fd3efa9c9b
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-19 14:07:26 -0300

    Section 4: Callback Functions & APIs

---
+++ weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+request({
+  // Using key from google developer, some changes might be needed!
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia&key=AIzaSyBPHa_STFlfzSDgjPoDOftEX3J_0St0BW4',
+  json: true
+}, (error, response, body) => {
+  console.log(body);
+});
