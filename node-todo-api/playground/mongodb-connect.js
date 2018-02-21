d4025126d84db97d3297d43d18d5bf741549b550
Marcos Schead <marcos.m4am@gmail.com>
2018-02-21 20:57:59 -0300

    Section 7: Connecting to Mongo and Writing Data

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://database:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log(err);
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Schead',
+    age: 26,
+    location: 'Florianopolis'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert user', err);
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+
+  client.close();
+});
+
