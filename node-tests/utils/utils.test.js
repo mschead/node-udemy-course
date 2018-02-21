bef8fb79028edcf77b92c3dc21eff97189df89cc
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-21 14:48:09 -0300

    Section 6: Testing Asynchronous Code

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -8,10 +8,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('should square a number', () => {
   var res = utils.square(3);
   
   expect(res).toBe(9).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (res) => {
+    expect(res).toBe(25).toBeA('number');
+    done();
+  });
 });
 
 it('should set firstName and lastName', () => {
