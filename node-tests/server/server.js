2b5633edbb813943e0d0d0c5276ec921638f6390
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-21 15:36:04 -0300

    Section 6: Testing Express Applications: Part II

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,24 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+      name: 'Mike',
+      age: 27
+    }, {
+      name: 'Schead',
+      age: 26
+    }, {
+      name: 'Jen',
+      age: 25
+    }]);
 });
 
 app.listen(3000);
+module.exports.app = app;
