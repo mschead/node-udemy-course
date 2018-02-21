41f7761acc6ce3ca350f11eb769089ea9ab01be5
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-21 15:49:21 -0300

    Section 6: Organizing Test With describe()

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -2,29 +2,34 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
   
-  expect(res).toBe(44).toBeA('number');
-});
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+      expect(res).toBe(44).toBeA('number');
+    });
   });
-});
-
-it('should square a number', () => {
-  var res = utils.square(3);
   
-  expect(res).toBe(9).toBeA('number');
-});
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  });
 
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (res) => {
-    expect(res).toBe(25).toBeA('number');
-    done();
+  it('should square a number', () => {
+    var res = utils.square(3);
+
+    expect(res).toBe(9).toBeA('number');
+  });
+
+  it('should async square a number', (done) => {
+    utils.asyncSquare(5, (res) => {
+      expect(res).toBe(25).toBeA('number');
+      done();
+    });
   });
 });
 
