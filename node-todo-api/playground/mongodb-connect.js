30d462e100e2cd0ba5307d47ef081ac6a4be66d7
Marcos Schead <marcos.m4am@gmail.com>
2018-02-21 22:26:04 -0300

    Section 7: The ObjectId

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://database:27017/TodoApp', (err, client) => {
   if (err) {
@@ -6,7 +7,7 @@ MongoClient.connect('mongodb://database:27017/TodoApp', (err, client) => {
     return console.log('Unable to connect to MongoDB server');
   }
   console.log('Connected to MongoDB server');
-  const db = client.db('TodoApp');
+  // const db = client.db('TodoApp');
 
   // db.collection('Todos').insertOne({
   //   text: 'Something to do',
@@ -19,17 +20,17 @@ MongoClient.connect('mongodb://database:27017/TodoApp', (err, client) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Schead',
-    age: 26,
-    location: 'Florianopolis'
-  }, (err, result) => {
-    if (err) {
-      return console.log('Unable to insert user', err);
-    }
+  // db.collection('Users').insertOne({
+  //   name: 'Schead',
+  //   age: 26,
+  //   location: 'Florianopolis'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
 
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   client.close();
 });
