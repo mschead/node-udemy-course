77b36221cbb90ffe1503cae3ceecdd1a300aef30
Marcos Schead <marcos@bridge.ufsc.br>
2018-02-22 13:07:05 -0300

    Section 7: Validators, Types, and Defaults

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,34 +5,46 @@ mongoose.connect('mongodb://database:27017/TodoApp');
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
-
 var otherTodo = new Todo({
-  text: 'Feed the cat',
-  completed: true,
-  completedAt: 123
+  text: 'Something to do'
 });
 
 otherTodo.save().then((doc) => {
   console.log(JSON.stringify(doc, undefined, 2));
 }, (e) => {
   console.log('Unable to save', e);
+});
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
+});
+
+var user = new User({
+  email: 'schead@example.com   '
+});
+
+user.save().then((doc) => {
+  console.log('User saved', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
 });
