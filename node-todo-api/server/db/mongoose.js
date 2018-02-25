8e47e09f951e1831b5e9856f77d77654ab4819b3
Marcos Schead <marcos.m4am@gmail.com>
2018-02-25 02:37:14 -0300

    Section 7: Deploy API to Heroku

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://database:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://database:27017/TodoApp');
 
 module.exports = { mongoose };
