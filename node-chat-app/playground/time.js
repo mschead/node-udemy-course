34b91aca974d96c340eb2e357ef886b428008b4c
Marcos Schead <marcos@bridge.ufsc.br>
2018-03-05 16:23:34 -0300

    Section 9: Printing Message Timestamps

--- node-chat-app/playground/time.js
+++ node-chat-app/playground/time.js
@@ -10,5 +10,9 @@ var moment = require('moment');
 // date.add(100, 'year').subtract(9, 'months');
 // console.log(date.format('MMM Do, YYYY'));
 
-var date = moment();
+var someTimestamp = moment.valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234;
+var date = moment(createdAt);
 console.log(date.format('h:mm a'));
