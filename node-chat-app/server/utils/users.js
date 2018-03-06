d9c3a1bf4148404d8308d849b115370a5e1754eb
Marcos Schead <marcos.m4am@gmail.com>
2018-03-05 22:54:26 -0300

    Section 9: Storing Users With ES6 Classes - Part II

--- node-chat-app/server/utils/users.js
+++ node-chat-app/server/utils/users.js
@@ -10,6 +10,7 @@
 // getUserList(room)
 
 class Users {
+
   constructor() {
     this.users = [];
   }
@@ -19,6 +20,28 @@ class Users {
     this.users.push(user);
     return user;
   }
+
+  removeUser(id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+
+  getUser(id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  getUserList(room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  }
+
 }
 
 module.exports = { Users };
