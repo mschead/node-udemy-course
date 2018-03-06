17e5948186dcc162bfe786ee67564f6301bb94cf
Marcos Schead <marcos@bridge.ufsc.br>
2018-03-06 17:19:49 -0300

    Section 10: Handling Errors and Awaiting Async Function

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -3,16 +3,28 @@
 
 const axios = require('axios');
 
-const getExchangeRate = (from, to) => {
-  return axios.get(`https://api.fixer.io/latest?base=${from}`).then((response) => {
-    return response.data.rates[to];
-  });
+const getExchangeRate = async (from, to) => {
+  try {
+    const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+    const rate = response.data.rates[to];
+
+    if (rate) {
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+  }
 }
 
-const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) => {
+const getCountries = async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map((country) => country.name);
-  });
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}.`);
+  }
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -35,6 +47,8 @@ const convertCurrencyAlt = async (from, to, amount) => {
   return `${amount} ${from} is worth ${exchangedAmount} ${to}. ${to} can be used in the following countries: ${countries.join(', ')}`;
 };
 
-convertCurrencyAlt('USD', 'CAD', 100).then((status) => {
+convertCurrencyAlt('USD', 'EUR', 100).then((status) => {
   console.log(status);
+}).catch((e) => {
+  console.log(e.message);
 });
