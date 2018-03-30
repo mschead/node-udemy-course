f05bf4009d521d1b01e57e03a2550ea8443690fa
Marcos Schead <marcos.m4am@gmail.com>
2018-03-30 18:37:08 -0300

    Section 11: Diving into ESLint: ESLint Airbnb Rules (3 of 3)

--- free-mini-courses/eslint-basics/app.js
+++ free-mini-courses/eslint-basics/app.js
@@ -1,4 +1,16 @@
-var bar = 10;
-const foo = bar;
+// const bar = 10;
 
-document.write(foo);
+// const work = new Promise((resolve, reject) => {
+//   resolve('Some data');
+// });
+
+// work.then((data) => {
+//   console.log(data);
+// });
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const jsx = <p>test</p>;
+
+ReactDOM.render(jsx, document.getElementById('app'));
