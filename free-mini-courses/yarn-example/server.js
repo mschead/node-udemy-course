aaf81dd8c9ce137019c7fde2e05b30f60b4c8a04
Marcos Schead <marcos.m4am@gmail.com>
2018-03-30 19:32:23 -0300

    Section 11: Learn Yarn - An npm Alternative

---
+++ free-mini-courses/yarn-example/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is my yarn project.');
+});
+
+app.listen(port, () => {
+  console.log(`The server is up on port ${port}!`);
+});
